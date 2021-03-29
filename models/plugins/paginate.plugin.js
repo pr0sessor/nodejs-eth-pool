a015a8274cb9b669743e7beecf525005c07fa5aa
pr0sessor <edgarfahey@hotmail.com>
2021-03-29 14:54:08 +0800

    fixes

--- models/plugins/paginate.plugin.js
+++ models/plugins/paginate.plugin.js
@@ -19,38 +19,38 @@ const paginate = (schema) => {
    * @returns {Promise<QueryResult>}
    */
   schema.statics.paginate = async function (filter, options) {
-    let sort = '';
+    let sort = ''
     if (options.sortBy) {
-      const sortingCriteria = [];
+      const sortingCriteria = []
       options.sortBy.split(',').forEach((sortOption) => {
-        const [key, order] = sortOption.split(':');
-        sortingCriteria.push((order === 'desc' ? '-' : '') + key);
-      });
-      sort = sortingCriteria.join(' ');
+        const [key, order] = sortOption.split(':')
+        sortingCriteria.push((order === 'desc' ? '-' : '') + key)
+      })
+      sort = sortingCriteria.join(' ')
     } else {
-      sort = 'createdAt';
+      sort = 'createdAt'
     }
 
-    const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
-    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
-    const skip = (page - 1) * limit;
+    const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10
+    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1
+    const skip = (page - 1) * limit
 
-    const countPromise = this.countDocuments(filter).exec();
-    const docsPromise = this.find(filter).sort(sort).skip(skip).limit(limit).exec();
+    const countPromise = this.countDocuments(filter).exec()
+    const docsPromise = this.find(filter).sort(sort).skip(skip).limit(limit).exec()
 
     return Promise.all([countPromise, docsPromise]).then((values) => {
-      const [totalResults, results] = values;
-      const totalPages = Math.ceil(totalResults / limit);
+      const [totalResults, results] = values
+      const totalPages = Math.ceil(totalResults / limit)
       const result = {
         results,
         page,
         limit,
         totalPages,
-        totalResults,
-      };
-      return Promise.resolve(result);
-    });
-  };
-};
+        totalResults
+      }
+      return Promise.resolve(result)
+    })
+  }
+}
 
-module.exports = paginate;
+module.exports = paginate
--- models/plugins/toJSON.plugin.js
+++ models/plugins/toJSON.plugin.js
@@ -6,29 +6,29 @@
  *  - replaces _id with id
  */
 const toJSON = (schema) => {
-  let transform;
+  let transform
   if (schema.options.toJSON && schema.options.toJSON.transform) {
-    transform = schema.options.toJSON.transform;
+    transform = schema.options.toJSON.transform
   }
 
   schema.options.toJSON = Object.assign(schema.options.toJSON || {}, {
-    transform(doc, ret, options) {
+    transform (doc, ret, options) {
       Object.keys(schema.paths).forEach((path) => {
         if (schema.paths[path].options && schema.paths[path].options.private) {
-          delete ret[path];
+          delete ret[path]
         }
-      });
+      })
 
-      ret.id = ret._id.toString();
-      delete ret._id;
-      delete ret.__v;
-      delete ret.createdAt;
-      delete ret.updatedAt;
+      ret.id = ret._id.toString()
+      delete ret._id
+      delete ret.__v
+      delete ret.createdAt
+      delete ret.updatedAt
       if (transform) {
-        return transform(doc, ret, options);
+        return transform(doc, ret, options)
       }
-    },
-  });
-};
+    }
+  })
+}
 
-module.exports = toJSON;
+module.exports = toJSON
